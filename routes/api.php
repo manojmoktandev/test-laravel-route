882d2f2941324ccc8827896f4b074d1bb6c32dd7
Manoj Moktan <developermanojmoktan@gmail.com>
2023-07-16 01:16:39 -0400

    update routes for web and api

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
+    // Keep in mind that prefix should be /api/v1.
+    Route::prefix('/v1')->group(function () {
+        Route::resource('tasks', Api\V1\TaskController::class);
+    });
+    // Add ONE line to assign 5 resource routes to TaskController
+    // Put one code line here below
+
+});
